65d112f03041345e02d90094f338a77e04293607
Eli Goldfine <manifed.markets@gmail.com>
2026-04-26 12:02:58 -0400

    Update ask.py

--- api/ask.py
+++ api/ask.py
@@ -1,6 +1,12 @@
 import json
+import os
+import sys
 from http.server import BaseHTTPRequestHandler
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
 from scripts.ask import ask
+
 
 class handler(BaseHTTPRequestHandler):
     def do_POST(self):
